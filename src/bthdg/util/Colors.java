b1b76b70d6a66a92bdc20b188bdf4e44d5411b1e
hedgebk <dmitrycar@gmail.com>
2015-02-05 02:59:07 +0200

    minor

--- src/bthdg/util/Colors.java
+++ src/bthdg/util/Colors.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 public class Colors {
     public static final Color LIGHT_RED = new Color(255, 0, 0, 51);
-    public static final Color LIGHT_BLUE = new Color(0, 0, 255, 51);
+    public static final Color LIGHT_BLUE = new Color(101, 98, 255);
     public static final Color TRANSP_GRAY = new Color(100, 100, 100, 50);
     public static final Color DARK_GREEN = new Color(0, 90, 0);
     public static final Color DARK_BLUE = new Color(0, 0, 120);
