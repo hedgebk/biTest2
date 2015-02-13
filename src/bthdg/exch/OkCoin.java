15948d21facda38cd6ea2d354d70c235ee6b8089
hedgebk <dmitrycar@gmail.com>
2015-02-13 16:18:09 +0200

    timeout increased

--- src/bthdg/exch/OkCoin.java
+++ src/bthdg/exch/OkCoin.java
@@ -20,8 +20,8 @@ public class OkCoin extends BaseExch {
     private static String PARTNER;
     public static boolean LOG_PARSE = false;
     public static boolean JOIN_SMALL_QUOTES = false;
-    public static final int CONNECT_TIMEOUT = 8000;
-    public static final int READ_TIMEOUT = 12000;
+    public static final int CONNECT_TIMEOUT = 12000;
+    public static final int READ_TIMEOUT = 20000;
 
     // supported pairs
     static final Pair[] PAIRS = {Pair.BTC_CNH, Pair.LTC_CNH };
