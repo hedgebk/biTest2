918f815922e49371d452d2eb0db1bf29d98daa75
hedgebk <dmitrycar@gmail.com>
2015-08-02 13:47:47 +0300

    first parse then process

--- src/bthdg/tres/TresLogProcessor.java
+++ src/bthdg/tres/TresLogProcessor.java
@@ -8,7 +8,7 @@ import bthdg.util.Utils;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,6 +17,9 @@ class TresLogProcessor extends Thread {
     private static final Pattern TRE_TRADE_PATTERN = Pattern.compile("onTrade\\[\\w+\\]\\: TradeData\\{amount=\\d+\\.\\d+, price=(\\d+\\.\\d+), time=(\\d+).+");
     // 1426040622351: State.onTrade(tData=TradeData{amount=5.00000, price=1831.00000, time=1426040623000, tid=0, type=ASK}) on NONE *********************************************
     private static final Pattern OSC_TRADE_PATTERN = Pattern.compile("\\d+: State.onTrade\\(tData=TradeData\\{amount=\\d+\\.\\d+, price=(\\d+\\.\\d+), time=(\\d+).+");
+    private static final Pattern FX_TRADE_PATTERN = Pattern.compile("EUR/USD,(\\d\\d\\d\\d)(\\d\\d)(\\d\\d) (\\d\\d):(\\d\\d):(\\d\\d).(\\d\\d\\d),(\\d+\\.\\d+),(\\d+.\\d+)");
+
+    private static final Calendar GMT_CALENDAR = Calendar.getInstance(TimeZone.getTimeZone("GMT"), Locale.ENGLISH);
 
     private final String m_logFilePattern;
     private TresExchData m_exchData;
@@ -38,114 +41,210 @@ class TresLogProcessor extends Thread {
 
             File dir = new File(dirPath);
             if (dir.isDirectory()) {
-                double totalTotal = 1;
-                File[] files = dir.listFiles();
-                log(files.length + " file(s) is directory " + dirPath);
-                int processed = 0;
-                int skipped = 0;
-                for (File file : files) {
-                    String name = file.getName();
-                    if (pattern.matcher(name).matches()) {
-                        log("next file to process: " + name);
-                        double avgTotal = processLines(file);
-                        processed++;
-                        totalTotal *= avgTotal;
-                        m_exchData = m_exchData.cloneClean();
-                    } else {
-                        skipped++;
-//                        log(" skip. not matches the pattern: " + filePattern);
-                    }
-                }
-                log("processed " + processed + " files. skipped " + skipped + " files");
-                log("totalTotal: " + totalTotal);
+                List<List<TradeData>> ticks = parseFiles(pattern, dir);
+                processAllTicks(ticks);
             } else {
                 log("is not a directory: " + dirPath);
             }
             Tres.showUI();
-        } catch (IOException e) {
+        } catch (Exception e) {
             err("Error in LogProcessor: " + e, e);
         }
     }
 
-    private double processLines(File file) throws IOException {
+    private void processAllTicks(List<List<TradeData>> allTicks) {
+        long startTime = System.currentTimeMillis();
+        Utils.DoubleDoubleAverageCalculator calc = new Utils.DoubleDoubleAverageCalculator();
+        for (List<TradeData> ticks : allTicks) {
+            double projected = processTicks(ticks);
+            calc.addValue(projected);
+        }
+        double averageProjected = calc.getAverage();
+        log("averageProjected: " + averageProjected);
+
+        long endTime = System.currentTimeMillis();
+        long timeTakes = endTime - startTime;
+        String takesStr = Utils.millisToDHMSStr(timeTakes);
+        log("takes " + takesStr);
+    }
+
+    private double processTicks(List<TradeData> ticks) {
+        Tres tres = m_exchData.m_tres;
+        // reset before iteration
+        tres.m_startTickMillis = Long.MAX_VALUE;
+        tres.m_lastTickMillis = 0;
+
+        for (TradeData tick : ticks) {
+            m_exchData.onTrade(tick);
+        }
+
+        Utils.DoubleDoubleAverageCalculator calc = new Utils.DoubleDoubleAverageCalculator();
+        for (PhaseData phaseData : m_exchData.m_phaseDatas) {
+            TresMaCalculator maCalculator = phaseData.m_maCalculator;
+            double total = maCalculator.calcToTal();
+//            log(" total=" + total);
+            calc.addValue(total);
+        }
+        double averageTotal = calc.getAverage();
+//        log("  avg=" + averageTotal);
+
+        long runningTimeMillis = tres.m_lastTickMillis - tres.m_startTickMillis;
+        double runningTimeDays = ((double) runningTimeMillis) / Utils.ONE_DAY_IN_MILLIS;
+//        String runningStr = Utils.millisToDHMSStr(runningTimeMillis);
+//        log("   running " + runningStr + "; runningTimeDays="+runningTimeDays);
+
+        double aDay = Math.pow(averageTotal, 1 / runningTimeDays);
+//        log("    aDay="+aDay);
+
+        // reset after iteration
+        m_exchData = m_exchData.cloneClean();
+
+        return aDay;
+    }
+
+    private List<List<TradeData>> parseFiles(Pattern pattern, File dir) throws Exception {
+        long startTime = System.currentTimeMillis();
+        List<List<TradeData>> ticks = new ArrayList<List<TradeData>>();
+        File[] files = dir.listFiles();
+        log(files.length + " file(s) is directory " + dir);
+        int parsed = 0;
+        int skipped = 0;
+        for (File file : files) {
+            String name = file.getName();
+            if (pattern.matcher(name).matches()) {
+                log("next file to parse: " + name);
+                List<TradeData> fileTicks = parseFile(file);
+                ticks.add(fileTicks);
+                parsed++;
+
+            } else {
+                skipped++;
+//                        log(" skip. not matches the pattern: " + filePattern);
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        long timeTakes = endTime - startTime;
+        String takesStr = Utils.millisToDHMSStr(timeTakes);
+
+        log("parsed " + parsed + " files. skipped " + skipped + " files. takes " + takesStr);
+        return ticks;
+    }
+
+    private List<TradeData> parseFile(File file) throws Exception {
         LineReader reader = new LineReader(file);
         try {
-            return processLines(reader);
+            return parseLines(reader);
         } finally {
             reader.close();
         }
     }
 
-    private double processLines(LineReader reader) {
+    private List<TradeData> parseLines(LineReader reader) throws IOException {
         BufferedLineReader blr = new BufferedLineReader(reader);
         try {
+            List<TradeData> ret = new ArrayList<TradeData>();
             long startTime = System.currentTimeMillis();
             long linesProcessed = 0;
             String line;
             while ((line = blr.getLine()) != null) {
-                processTheLine(line);
+                TradeData tData = parseTheLine(line);
+                if(tData != null) {
+                    ret.add(tData);
+                }
                 blr.removeLine();
                 linesProcessed++;
             }
             long endTime = System.currentTimeMillis();
             long timeTakes = endTime - startTime;
             log("processed " + linesProcessed + " lines in " + timeTakes + " ms (" + (linesProcessed * 1000 / timeTakes) + " lines/s)");
-
-            Utils.DoubleAverageCalculator calc = new Utils.DoubleDoubleAverageCalculator();
-            for (PhaseData phaseData : m_exchData.m_phaseDatas) {
-                TresMaCalculator maCalculator = phaseData.m_maCalculator;
-                double total = maCalculator.calcToTal();
-                log(" total=" + total);
-                calc.addValue(total);
-            }
-            double averageTotal = calc.getAverage();
-            log("  avg=" + averageTotal);
-            return averageTotal;
-        } catch (Throwable t) {
-            err("Error processing line: " + t, t);
-            return 1;
+            return ret;
+        } finally {
+            blr.close();
         }
     }
 
-    private void processTheLine(String line) {
-        // onTrade[OKCOIN]: TradeData{amount=0.01000, price=1766.62000, time=1437739761000, tid=0, type=BID}
+    private TradeData parseTheLine(String line) {
         if (line.startsWith("onTrade[") && line.contains("]: TradeData{")) {
-            processTradeLine(line);
+            return parseTradeLine(line);
         } else if (line.contains("State.onTrade(")) {
-            processOscTradeLine(line);
+            return parseOscTradeLine(line);
+        } else if (line.startsWith("EUR/USD,")) { // fx
+            return parseFxTradeLine(line);
+        }
+        return null;
+    }
+
+    private TradeData parseFxTradeLine(String line) {
+        // EUR/USD,20150601 00:00:00.859,1.0957,1.09579
+        Matcher matcher = FX_TRADE_PATTERN.matcher(line);
+        if (matcher.matches()) {
+            String yearStr = matcher.group(1);
+            String monthStr = matcher.group(2);
+            String dayStr = matcher.group(3);
+            String hourStr = matcher.group(4);
+            String minStr = matcher.group(5);
+            String secStr = matcher.group(6);
+            String millisStr = matcher.group(7);
+            String bidStr = matcher.group(8);
+            String askStr = matcher.group(9);
+//            log("GOT TRADE: yearStr=" + yearStr + "; monthStr=" + monthStr + "; dayStr=" + dayStr + "; hourStr=" + hourStr + "; minStr=" + minStr + "; secStr=" + secStr + "; millisStr=" + millisStr + "; bidStr=" + bidStr + "; askStr=" + askStr);
+
+            int year = Integer.parseInt(yearStr);
+            int month = Integer.parseInt(monthStr);
+            int day = Integer.parseInt(dayStr);
+            int hour = Integer.parseInt(hourStr);
+            int min = Integer.parseInt(minStr);
+            int sec = Integer.parseInt(secStr);
+            int millis = Integer.parseInt(millisStr);
+            double bid = Double.parseDouble(bidStr);
+            double ask = Double.parseDouble(askStr);
+//            log(" parsed: year=" + year + "; month=" + month + "; day=" + day + "; hour=" + hour + "; min=" + min + "; sec=" + sec + "; millis=" + millis + "; bid=" + bid + "; ask=" + ask);
+
+            GMT_CALENDAR.set(year, month, day, hour, min, sec);
+            GMT_CALENDAR.set(Calendar.MILLISECOND, millis);
+            long timestamp = GMT_CALENDAR.getTimeInMillis();
+            double mid = (bid + ask) / 2;
+
+            TradeData tradeData = new TradeData(0, mid, timestamp);
+            return tradeData;
+        } else {
+            throw new RuntimeException("not matched FX_TRADE_PATTERN line: " + line);
+//            log("not matched FX_TRADE_PATTERN line: " + line);
         }
     }
 
-    private void processOscTradeLine(String line) {
+    private TradeData parseOscTradeLine(String line) {
         // 1426040622351: State.onTrade(tData=TradeData{amount=5.00000, price=1831.00000, time=1426040623000, tid=0, type=ASK}) on NONE *********************************************
         Matcher matcher = OSC_TRADE_PATTERN.matcher(line);
         if (matcher.matches()) {
             String priceStr = matcher.group(1);
             String millisStr = matcher.group(2);
 //                log("GOT TRADE: millisStr=" + millisStr + "; priceStr=" + priceStr);
-            processTrade(millisStr, priceStr);
+            TradeData tradeData = parseTrade(millisStr, priceStr);
+            return tradeData;
         } else {
 //                throw new RuntimeException("not matched OSC_TRADE_PATTERN line: " + line);
             log("not matched OSC_TRADE_PATTERN line: " + line);
+            return null;
         }
     }
 
-    private void processTrade(String millisStr, String priceStr) {
-        long millis = Long.parseLong(millisStr);
-        double price = Double.parseDouble(priceStr);
-        TradeData tradeData = new TradeData(0, price, millis);
-        m_exchData.onTrade(tradeData);
-    }
-
-    private void processTradeLine(String line) {
+    private TradeData parseTradeLine(String line) {
         Matcher matcher = TRE_TRADE_PATTERN.matcher(line);
         if (matcher.matches()) {
             String priceStr = matcher.group(1);
             String timeStr = matcher.group(2);
 //                log("GOT TRADE: timeStr=" + timeStr + "; priceStr=" + priceStr + "; amountStr=" + amountStr);
-            processTrade(timeStr, priceStr);
+            TradeData tradeData = parseTrade(timeStr, priceStr);
+            return tradeData;
         } else {
             throw new RuntimeException("not matched TRE_TRADE_PATTERN line: " + line);
         }
+    }
+
+    private TradeData parseTrade(String millisStr, String priceStr) {
+        long millis = Long.parseLong(millisStr);
+        double price = Double.parseDouble(priceStr);
+        return new TradeData(0, price, millis);
     }
 }
