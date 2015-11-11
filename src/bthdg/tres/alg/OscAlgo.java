a26bd0763ffa189da4ea35b2632a1dea1c09db6a
hedgebk <dmitrycar@gmail.com>
2015-11-11 02:34:53 +0200

    direction line added

--- src/bthdg/tres/alg/OscAlgo.java
+++ src/bthdg/tres/alg/OscAlgo.java
@@ -1,17 +1,43 @@
 package bthdg.tres.alg;
 
 import bthdg.exch.Direction;
+import bthdg.tres.ChartPoint;
 import bthdg.tres.TresExchData;
 import bthdg.tres.TresOscCalculator;
 import bthdg.tres.ind.OscIndicator;
+import bthdg.tres.ind.TresIndicator;
+
+import java.awt.*;
 
 public class OscAlgo extends TresAlgo {
     final OscIndicator m_oscIndicator;
+    private final DirectionIndicator m_directionIndicator;
 
     public OscAlgo(TresExchData exchData) {
         super("OSC", exchData);
-        m_oscIndicator = new OscIndicator(this);
+        m_oscIndicator = new OscIndicator(this) {
+            @Override public void addBar(ChartPoint chartPoint) {
+                super.addBar(chartPoint);
+                ChartPoint lastPoint = getLastPoint();
+                if (lastPoint != null) {
+                    double value = getDirectionAdjustedByPeakWatchers(m_oscIndicator);
+                    long millis = lastPoint.m_millis;
+                    ChartPoint andPoint = new ChartPoint(millis, value);
+                    m_directionIndicator.addBar(andPoint);
+
+//                    Direction direction = getDirection();
+//                    if (direction != m_lastDirection) {
+//                        notifyListener();
+//                    }
+//                    m_lastDirection = direction;
+                }
+            }
+
+        };
         m_indicators.add(m_oscIndicator);
+
+        m_directionIndicator = new DirectionIndicator(this);
+        m_indicators.add(m_directionIndicator);
     }
 
     @Override public String getRunAlgoParams() {
@@ -27,4 +53,18 @@ public class OscAlgo extends TresAlgo {
         return getDirectionAdjustedByPeakWatchers(m_oscIndicator);
     }
     @Override public Direction getDirection() { return m_oscIndicator.m_peakWatcher.m_avgPeakCalculator.m_direction; } // UP/DOWN
+
+
+    public static class DirectionIndicator extends TresIndicator {
+        public static double PEAK_TOLERANCE = 0.06470;
+
+        public DirectionIndicator(TresAlgo algo) {
+            super("+", PEAK_TOLERANCE, algo);
+        }
+
+        @Override protected boolean countPeaks() { return false; }
+        @Override public TresPhasedIndicator createPhasedInt(TresExchData exchData, int phaseIndex) { return null; }
+        @Override public Color getColor() { return Color.red; }
+        @Override public Color getPeakColor() { return Color.red; }
+    }
 }
