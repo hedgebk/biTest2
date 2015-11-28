ebeef341ac7e93947fe6bbd0595b0b00808d0d78
hedgebk <dmitrycar@gmail.com>
2015-11-29 00:41:14 +0200

    jetty 9

--- src/bthdg/ehs/EmbeddedHttpServer.java
+++ src/bthdg/ehs/EmbeddedHttpServer.java
@@ -1,38 +1,44 @@
 package bthdg.ehs;
 
+import bthdg.Log;
+import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class EmbeddedHttpServer implements Runnable {
     private final Server m_server;
 
-    public int getPort() { return m_server.getConnectors()[0].getLocalPort(); }
+    private static void log(String s) { Log.log(s); }
+
+    public int getPort() {
+        Connector[] connectors = m_server.getConnectors();
+        Connector connector = connectors[0];
+List<String> protocols = connector.getProtocols();
+log("protocols=" + protocols);
+        return ((ServerConnector)connector).getLocalPort();
+    }
     public boolean notStarted() { return !m_server.isStarted(); }
 
     public static void main(String[] args) {
         // An embedded http server
-        EmbeddedHttpServer embeddedServer = new EmbeddedHttpServer();
+        EmbeddedHttpServer embeddedServer = new EmbeddedHttpServer(0);
         (new Thread(embeddedServer)).start();
-        System.out.println("Starting local web server");
+        log("Starting local web server");
 
         // wait for embedded http server to start
         while (embeddedServer.notStarted()) {}
 
-        System.out.println("embeddedServer Started on port "+embeddedServer.getPort());
+        log("embeddedServer Started on port " + embeddedServer.getPort());
 
-        try {
-            String browseLocation = "http://localhost:" + embeddedServer.getPort();
-            System.out.println("Opening browser to go here: " + browseLocation);
-            browseTo(browseLocation);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        embeddedServer.openBrowser();
 
         try {
             Thread.sleep(600000);
@@ -41,25 +47,37 @@ public class EmbeddedHttpServer implements Runnable {
         }
     }
 
+    public void openBrowser() {
+        try {
+            String browseLocation = "http://localhost:" + getPort();
+            log("Opening browser to go here: " + browseLocation);
+            browseTo(browseLocation);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     private static void browseTo(String url) throws IOException {
         Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
     }
 
-    public EmbeddedHttpServer() {
-        m_server = new Server(0);
+    public EmbeddedHttpServer(int port) {
+        m_server = new Server(port);
         m_server.setHandler(new AbstractHandler() {
-            @Override
-            public void handle(String target, Request baseRequest, HttpServletRequest request,
-                               HttpServletResponse response) throws IOException, ServletException {
-                System.out.println("handle request: " + target);
-                response.setContentType("text/html;charset=utf-8");
-                response.setStatus(HttpServletResponse.SC_OK);
+            @Override public void handle(String target, Request baseRequest,
+                                         HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+                log("handle request: " + target);
                 baseRequest.setHandled(true);
-//                if (request.getParameter("code") != null && !request.getParameter("code").isEmpty()) {
-                response.getWriter().println("<html><body><h1>TEST</h1>"
-                        + "<p>this is test.</p></body></html>");
+                handleRequest(request, response);
             }
         });
+    }
+
+    protected void handleRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+// if (request.getParameter("code") != null && !request.getParameter("code").isEmpty()) {
+        response.getWriter().println("<html><body><h1>TEST</h1><p>this is test.</p></body></html>");
     }
 
     @Override public void run() {
@@ -68,6 +86,16 @@ public class EmbeddedHttpServer implements Runnable {
             m_server.join();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public void startServer() throws InterruptedException {
+        (new Thread(this)).start();
+        log("Starting local web server");
+
+        // wait for embedded http server to start
+        while (notStarted()) {
+            Thread.sleep(100);
         }
     }
 }
