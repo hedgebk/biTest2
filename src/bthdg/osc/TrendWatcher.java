6cffe869dea192e8662797964ba1fcd38cb60e47
hedgebk <dmitrycar@gmail.com>
2016-03-13 01:12:01 +0200

    catch Infinite peakCandidate

--- src/bthdg/osc/TrendWatcher.java
+++ src/bthdg/osc/TrendWatcher.java
@@ -1,5 +1,6 @@
 package bthdg.osc;
 
+import bthdg.Log;
 import bthdg.exch.Direction;
 
 public abstract class TrendWatcher<O> {
@@ -10,6 +11,8 @@ public abstract class TrendWatcher<O> {
     private O m_lastValue;
 
     protected abstract double toDouble(O value);
+
+    protected static void log(String s) { Log.log(s); }
 
     public TrendWatcher(double tolerance) {
         m_tolerance = tolerance;
@@ -27,29 +30,50 @@ public abstract class TrendWatcher<O> {
                 if (diff > tolerance) {
                     m_direction = Direction.FORWARD;
                     m_peakCandidate = value;
+                    if(Double.isInfinite(toDouble(m_peakCandidate))) {
+                        log("Error: peakCandidate is Infinite 1");
+                    }
                 } else if (diff < -tolerance) {
                     m_direction = Direction.BACKWARD;
                     m_peakCandidate = value;
+                    if(Double.isInfinite(toDouble(m_peakCandidate))) {
+                        log("Error: peakCandidate is Infinite 2");
+                    }
                 }
             } else {
                 double doublePeakCandidate = toDouble(m_peakCandidate);
+                if(Double.isInfinite(doublePeakCandidate)) {
+                    log("Error: peakCandidate is Infinite 3");
+                }
                 double diff = doubleValue - doublePeakCandidate;
                 if (m_direction == Direction.FORWARD) { // going up
                     if (diff > 0) { // new value is above current peak candidate ?
                         m_peakCandidate = value; // got new peak candidate
+                        if(Double.isInfinite(toDouble(m_peakCandidate))) {
+                            log("Error: peakCandidate is Infinite 4");
+                        }
                     } else if (diff < -tolerance) { // new value is below the tolerance level ?
                         m_direction = Direction.BACKWARD; // confirmed new peak - now going down
                         m_peak = m_peakCandidate;
                         m_peakCandidate = value;
+                        if(Double.isInfinite(toDouble(m_peakCandidate))) {
+                            log("Error: peakCandidate is Infinite 5");
+                        }
                         onNewPeak(m_peak, value);
                     }
                 } else if (m_direction == Direction.BACKWARD) { // going down
                     if (diff < 0) { // new value is below current peak candidate ?
                         m_peakCandidate = value; // got new peak candidate
+                        if(Double.isInfinite(toDouble(m_peakCandidate))) {
+                            log("Error: peakCandidate is Infinite 6");
+                        }
                     } else if (diff > tolerance) { // new value is above the tolerance level ?
                         m_direction = Direction.FORWARD; // confirmed new peak - now going up
                         m_peak = m_peakCandidate;
                         m_peakCandidate = value;
+                        if(Double.isInfinite(toDouble(m_peakCandidate))) {
+                            log("Error: peakCandidate is Infinite 7");
+                        }
                         onNewPeak(m_peak, value);
                     }
                 }
