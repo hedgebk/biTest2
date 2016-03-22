89b96461b2cee0ebca00ffc7c79275308eead7d4
hedgebk <dmitrycar@gmail.com>
2016-03-23 00:01:07 +0200

    EWO_N algo added

--- src/bthdg/tres/alg/EwoAlgo.java
+++ src/bthdg/tres/alg/EwoAlgo.java
@@ -1,39 +1,29 @@
 package bthdg.tres.alg;
 
-import bthdg.ChartAxe;
 import bthdg.exch.Direction;
 import bthdg.exch.TradeDataLight;
 import bthdg.tres.ChartPoint;
 import bthdg.tres.TresExchData;
-import bthdg.tres.ind.TresIndicator;
-import bthdg.tres.ind.TripleEmaIndicator;
-import bthdg.tres.ind.VelocityIndicator;
-import bthdg.tres.ind.ZeroLeveler;
+import bthdg.tres.ind.*;
 import bthdg.util.Colors;
 
 import java.awt.*;
 
 // based on Elliot Wave Oscillator
-public class EwoAlgo extends TresAlgo {
+public abstract class EwoAlgo extends TresAlgo {
     public static double FAST_EMA_SIZE = 5;
     public static double SLOW_EMA_SIZE = 35;
-    public static double START_ZERO_LEVEL = 0.000001;
-    public static double VELOCITY_SIZE = 1.0;
 
-//    private final EmaIndicator m_fastEma;
+    //    private final EmaIndicator m_fastEma;
 //    private final EmaIndicator m_slowEma;
 //    private final TresIndicator m_ewoIndicator;
     private final TripleEmaIndicator m_fastTEma;
     private final TripleEmaIndicator m_slowTEma;
     private final TresIndicator m_ewo2Indicator;
-    private final VelocityIndicator m_ewo2VelocityIndicator;
-    private final ZeroLeveler m_zeroLeveler;
-    private final TresIndicator m_resIndicator;
-//    private boolean m_changed;
+    //    private boolean m_changed;
 //    private double m_ewo;
     private boolean m_changed2;
     private double m_ewo2;
-    private double m_updated;
 
     public EwoAlgo(TresExchData tresExchData) {
         this("Ewo", tresExchData);
@@ -41,9 +31,7 @@ public class EwoAlgo extends TresAlgo {
 
     public EwoAlgo(String name, TresExchData tresExchData) {
         super(name, tresExchData);
-        final long barSizeMillis = tresExchData.m_tres.m_barSizeMillis;
-
-        m_zeroLeveler = new ZeroLeveler(START_ZERO_LEVEL);
+//        final long barSizeMillis = tresExchData.m_tres.m_barSizeMillis;
 
 //        m_fastEma = new EmaIndicator("ef", this, FAST_EMA_SIZE) {
 //            @Override public void addBar(ChartPoint chartPoint) {
@@ -94,63 +82,22 @@ public class EwoAlgo extends TresAlgo {
             @Override public TresIndicator.TresPhasedIndicator createPhasedInt(TresExchData exchData, int phaseIndex) { return null; }
             @Override public Color getColor() { return Colors.SKY; }
             @Override protected boolean countPeaks() { return false; }
-            @Override protected void preDraw(Graphics g, ChartAxe xTimeAxe, ChartAxe yAxe) {
-                g.setColor(getColor());
-                int y = yAxe.getPointReverse(0);
-                g.drawLine(xTimeAxe.getPoint(xTimeAxe.m_min), y, xTimeAxe.getPoint(xTimeAxe.m_max), y);
-            }
+            @Override protected boolean drawZeroLine() { return true; }
             @Override public void addBar(ChartPoint chartPoint) {
                 super.addBar(chartPoint);
                 ChartPoint lastPoint = getLastPoint();
-                m_ewo2VelocityIndicator.addBar(lastPoint);
+                onEwo2(lastPoint);
             }
         };
         m_indicators.add(m_ewo2Indicator);
-
-        long veloсitySize = (long) (barSizeMillis * VELOCITY_SIZE);
-        m_ewo2VelocityIndicator = new VelocityIndicator(this, "vel", veloсitySize, 0) {
-            @Override protected boolean countPeaks() { return false; }
-            @Override public void addBar(ChartPoint chartPoint) {
-                super.addBar(chartPoint);
-                ChartPoint lastPoint = getLastPoint();
-                if (lastPoint != null) {
-                    double value = lastPoint.m_value;
-                    double updated = m_zeroLeveler.update(value);
-
-                    if (m_updated != updated) {
-                        m_updated = updated;
-
-                        ChartPoint point = new ChartPoint(lastPoint.m_millis, updated);
-                        m_resIndicator.addBar(point);
-                    }
-
-                }
-            }
-        };
-        m_indicators.add(m_ewo2VelocityIndicator);
-
-        m_resIndicator = new TresIndicator( "res", 0, this ) {
-            @Override public TresIndicator.TresPhasedIndicator createPhasedInt(TresExchData exchData, int phaseIndex) { return null; }
-            @Override public Color getColor() { return Colors.LIGHT_MAGNETA; }
-            @Override protected boolean countPeaks() { return false; }
-//            @Override protected void preDraw(Graphics g, ChartAxe xTimeAxe, ChartAxe yAxe) {
-//                g.setColor(getColor());
-//                int y = yAxe.getPointReverse(0);
-//                g.drawLine(xTimeAxe.getPoint(xTimeAxe.m_min), y, xTimeAxe.getPoint(xTimeAxe.m_max), y);
-//            }
-        };
-        m_indicators.add(m_resIndicator);
     }
+
+    protected void onEwo2(ChartPoint lastPoint) {}
 
     @Override public double lastTickPrice() { return m_fastTEma.lastTickPrice(); }
     @Override public long lastTickTime() { return m_fastTEma.lastTickTime(); }
     @Override public Color getColor() { return Color.LIGHT_GRAY; }
-    @Override public String getRunAlgoParams() { return "EWO"; }
 
-    @Override public double getDirectionAdjusted() {
-        return m_updated;
-//        return m_ewo2;
-    }
     @Override public Direction getDirection() {
         double dir = getDirectionAdjusted();
         return (dir == 0) ? null : Direction.get(dir);
@@ -196,6 +143,125 @@ public class EwoAlgo extends TresAlgo {
                 m_ewo2Indicator.addBar(point);
 
                 notifyListener();
+            }
+        }
+    }
+
+
+    //======================================================================
+    public static class Old extends EwoAlgo {
+        public static double START_ZERO_LEVEL = 0.000001;
+        public static double VELOCITY_SIZE = 1.0;
+        public static double SMOOTH_RATE = 0.1;
+
+        //    private final SmoochedIndicator m_smoochedEwo2Indicator;
+        private final VelocityIndicator m_ewo2VelocityIndicator;
+        private final SmoochedIndicator m_smoochedEwo2VelocityIndicator;
+        private final ZeroLeveler m_zeroLeveler;
+        private final TresIndicator m_resIndicator;
+        private double m_updated;
+
+        @Override public double getDirectionAdjusted() { return m_updated; }
+        @Override public String getRunAlgoParams() { return "EWO"; }
+
+        public Old(TresExchData tresExchData) {
+            super("Ewo", tresExchData);
+            final long barSizeMillis = tresExchData.m_tres.m_barSizeMillis;
+
+            m_zeroLeveler = new ZeroLeveler(START_ZERO_LEVEL);
+
+//        long frameSizeMillis = (long) (0.1 * barSizeMillis);
+//        m_smoochedEwo2Indicator = new SmoochedIndicator(this, "sm", frameSizeMillis, 0) {
+//            @Override public Color getColor() { return Color.lightGray; }
+//            @Override protected boolean countPeaks() { return false; }
+//            @Override public void addBar(ChartPoint chartPoint) {
+//                super.addBar(chartPoint);
+//                m_ewo2VelocityIndicator.addBar(getLastPoint());
+//            }
+//        };
+//        m_indicators.add(m_smoochedEwo2Indicator);
+
+            long velocitySize = (long) (barSizeMillis * VELOCITY_SIZE);
+            m_ewo2VelocityIndicator = new VelocityIndicator(this, "vel", velocitySize, 0) {
+                @Override protected boolean countPeaks() { return false; }
+                @Override public void addBar(ChartPoint chartPoint) {
+                    super.addBar(chartPoint);
+                    m_smoochedEwo2VelocityIndicator.addBar(getLastPoint());
+                }
+            };
+            m_indicators.add(m_ewo2VelocityIndicator);
+
+            long frameSizeMillis2 = (long) (SMOOTH_RATE * barSizeMillis);
+            m_smoochedEwo2VelocityIndicator = new SmoochedIndicator(this, "velsm", frameSizeMillis2, 0) {
+                @Override public Color getColor() { return Color.blue; }
+                @Override protected boolean countPeaks() { return false; }
+                @Override protected boolean drawZeroLine() { return true; }
+                @Override public void addBar(ChartPoint chartPoint) {
+                    super.addBar(chartPoint);
+                    ChartPoint lastPoint = getLastPoint();
+                    if (lastPoint != null) {
+                        double value = lastPoint.m_value;
+                        double updated = m_zeroLeveler.update(value);
+                        if (m_updated != updated) {
+                            m_updated = updated;
+                            ChartPoint point = new ChartPoint(lastPoint.m_millis, updated);
+                            m_resIndicator.addBar(point);
+                        }
+                    }
+                }
+            };
+            m_indicators.add(m_smoochedEwo2VelocityIndicator);
+
+            m_resIndicator = new TresIndicator( "res", 0, this ) {
+                @Override public TresIndicator.TresPhasedIndicator createPhasedInt(TresExchData exchData, int phaseIndex) { return null; }
+                @Override public Color getColor() { return Colors.LIGHT_MAGNETA; }
+                @Override protected boolean countPeaks() { return false; }
+                @Override protected boolean drawZeroLine() { return true; }
+            };
+            m_indicators.add(m_resIndicator);
+        }
+
+        @Override protected void onEwo2(ChartPoint lastPoint) {
+//                m_smoochedEwo2Indicator.addBar(lastPoint);
+            m_ewo2VelocityIndicator.addBar(lastPoint);
+        }
+    }
+
+
+    //======================================================================
+    public static class New extends EwoAlgo {
+        public static double START_ZERO_LEVEL = 0.1;
+
+        private final ZeroLeveler m_zeroLeveler;
+        private final TresIndicator m_resIndicator;
+        private double m_updated;
+
+        @Override public double getDirectionAdjusted() { return m_updated; }
+        @Override public String getRunAlgoParams() { return "EWOn"; }
+
+        public New(TresExchData tresExchData) {
+            super("EwoN", tresExchData);
+            m_zeroLeveler = new ZeroLeveler(START_ZERO_LEVEL);
+
+            m_resIndicator = new TresIndicator( "res", 0, this ) {
+                @Override public TresIndicator.TresPhasedIndicator createPhasedInt(TresExchData exchData, int phaseIndex) { return null; }
+                @Override public Color getColor() { return Colors.LIGHT_MAGNETA; }
+                @Override protected boolean countPeaks() { return false; }
+                @Override protected boolean drawZeroLine() { return true; }
+            };
+            m_indicators.add(m_resIndicator);
+        }
+
+        @Override protected void onEwo2(ChartPoint lastPoint) {
+//            m_ewo2VelocityIndicator.addBar(lastPoint);
+            if (lastPoint != null) {
+                double value = lastPoint.m_value;
+                double updated = m_zeroLeveler.update(value);
+                if (m_updated != updated) {
+                    m_updated = updated;
+                    ChartPoint point = new ChartPoint(lastPoint.m_millis, updated);
+                    m_resIndicator.addBar(point);
+                }
             }
         }
     }
