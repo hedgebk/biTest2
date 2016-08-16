78d1e4c6c52541a6382d7d085534d235d27d779e
hedgebk <dmitrycar@gmail.com>
2016-08-16 03:59:15 +0300

    OHLCFrameCalculator refactored

--- src/bthdg/calc/OHLCCalculator.java
+++ src/bthdg/calc/OHLCCalculator.java
@@ -1,5 +1,7 @@
 package bthdg.calc;
 
+import java.util.LinkedList;
+
 public class OHLCCalculator extends BarCalculator {
     protected OHLCTick m_tick;
 
@@ -17,4 +19,28 @@ public class OHLCCalculator extends BarCalculator {
         m_tick = new OHLCTick(barStart, barEnd);
     }
     @Override protected void finishCurrentBar(long time, double price) {}
+
+
+    public static class OHLCFrameCalculator extends OHLCCalculator {
+        protected final int m_length;
+        protected final LinkedList<OHLCTick> m_bars = new LinkedList<OHLCTick>();
+        protected boolean m_filled;
+
+        public OHLCFrameCalculator(int length, long barSize, long barsMillisOffset) {
+            super(barSize, barsMillisOffset);
+            m_length = length;
+        }
+
+        @Override protected void startNewBar(long barStart, long barEnd) {
+            super.startNewBar(barStart, barEnd);
+            m_bars.addLast(m_tick);
+            int size = m_bars.size();
+            if (size == m_length) {
+                m_filled = true;
+            }
+            if (size > m_length) {
+                m_bars.removeFirst();
+            }
+        }
+    }
 }
