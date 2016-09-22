844303c171459c01d844024a82973c8c72c711fa
hedgebk <dmitrycar@gmail.com>
2016-09-22 03:48:52 +0300

    separate queue for ExecTasks

--- src/bthdg/tres/TresTaskQueueProcessor.java
+++ src/bthdg/tres/TresTaskQueueProcessor.java
@@ -8,6 +8,7 @@ import java.util.LinkedList;
 public class TresTaskQueueProcessor extends TaskQueueProcessor {
     protected final LinkedList<BaseOrderTask> m_tradeTasksQueue = new LinkedList<BaseOrderTask>();
     protected final LinkedList<BaseOrderTask> m_topTasksQueue = new LinkedList<BaseOrderTask>();
+    protected final LinkedList<BaseOrderTask> m_execTasksQueue = new LinkedList<BaseOrderTask>();
 
     @Override protected LinkedList<BaseOrderTask> getQueue(BaseOrderTask task) {
         if (task instanceof BaseExecutor.TradeTask) {
@@ -16,15 +17,23 @@ public class TresTaskQueueProcessor extends TaskQueueProcessor {
         if (task instanceof BaseExecutor.TopTask) {
             return m_topTasksQueue;
         }
+        if (task instanceof TresExecutor.ExecTask) {
+            return m_execTasksQueue;
+        }
         return super.getQueue(task);
     }
 
     @Override protected BaseOrderTask poolTask() throws InterruptedException {
+        // process all trades/top tasks first - mkt data should be up to date
         BaseOrderTask ret = m_tradeTasksQueue.pollFirst();
         if (ret == null) {
             ret = m_topTasksQueue.pollFirst();
             if (ret == null) {
-                ret = super.poolTask();
+                // then process our execs
+                ret = m_execTasksQueue.pollFirst();
+                if (ret == null) {
+                    ret = super.poolTask();
+                }
             }
         }
         return ret;
